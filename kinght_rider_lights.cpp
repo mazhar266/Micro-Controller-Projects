5648adb9f56fcf2ce53ba9786d78bdeb00ec5fa9
Mazhar Ahmed <mazhar266@gmail.com>
2022-11-09 10:34:45 +0600

    Create kinght_rider_lights.cpp

---
+++ kinght_rider_lights.cpp
@@ -0,0 +1,45 @@
+int i = 0;
+int incr = 1;
+int leds[6] = {8, 9, 10, 11, 12, 13};
+
+void setup() {
+  // make all pins as output
+  for(int j = 0; j < 6; j++)
+  {
+    pinMode(leds[j], OUTPUT);
+  }
+}
+
+void loop() {
+  // first turn the selected LED on
+  digitalWrite(leds[i], HIGH);
+
+  // turn others off
+  for(int j = 0; j < 6; j++)
+  {
+    if (j == i)
+    {
+      // don't turn off selected LED
+      continue;
+    }
+    digitalWrite(leds[j], LOW);
+  }
+
+  // next step
+  i += incr;
+
+  // make negative at the last moment
+  if (i == 5 && incr == 1)
+  {
+    incr = -1;
+  }
+
+  // make positive at the first moment
+  if (i == 0 && incr == -1)
+  {
+    incr = 1;
+  }
+
+  // now delay with motion tween
+  delay(50 + (sin((2 * 3.1416 * i) / 5) * 30));
+}
